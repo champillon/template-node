25cbb78dfcf4395a987443c0e2fe9ec673acb070
Passapong Thaithatgoon <champillon@rurouni.local>
2022-12-16 15:34:10 +0700

    added prom-client, response-time

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var express = require('express');
+import client from 'prom-client'
+import express from 'express';
+
 var app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics
+
+collectDefaultMetrics()
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/metrics', async (req, res) => {
+  res.set("Content-Type", client.register.contentType)
+
+  return res.send(await client.register.metrics())
+})
+
 app.listen(4000, function () {
   console.log('Example app listening on port 3000!');
 });
