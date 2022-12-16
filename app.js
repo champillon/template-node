0ab3ac289e925dcb240b7af7a9e81122f0a16c64
Passapong Thaithatgoon <champillon@rurouni.local>
2022-12-16 16:09:21 +0700

    added counter and histrogram

--- app.js
+++ app.js
@@ -6,8 +6,32 @@ const collectDefaultMetrics = client.collectDefaultMetrics
 
 collectDefaultMetrics()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const counter = new client.Counter({
+  name: 'node_request_operations_total',
+  help: 'The total number of processed requests'
+});
+
+const histogram = new client.Histogram({
+  name: 'node_request_duration_seconds',
+  help: 'Histogram for the duration in seconds.',
+  buckets: [1, 2, 5, 6, 10]
+});
+
+app.get('/', (req, res) => {
+
+  //Simulate a sleep
+  var start = new Date()
+  var simulateTime = 1000
+
+  setTimeout(function(argument) {
+    // execution time simulated with setTimeout function
+    var end = new Date() - start
+    histogram.observe(end / 1000); //convert to seconds
+  }, simulateTime)
+
+  counter.inc();
+  
+  res.send('Hello world\n');
 });
 
 app.get('/metrics', async (req, res) => {
